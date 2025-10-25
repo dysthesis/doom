88524bb62c9eee4430f4cf10c90c83167f51df9d
Dysthesis <antheoraviel@protonmail.com>
2025-10-25 16:35:16 +1100

    Agent-shell

--- packages.el
+++ packages.el
@@ -45,6 +45,10 @@
 
 (unpin! org)
 (package! typst-ts-mode)
+
+(package! shell-maker)
+(package! acp :recipe (:host github :repo "xenodium/acp.el"))
+(package! agent-shell :recipe (:host github :repo "xenodium/agent-shell"))
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
 ;; https://github.com/radian-software/straight.el#the-recipe-format
